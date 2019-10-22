a38478689963e3534c2a57b9880a01cbbe8a2508
NikitaLem <saintsuigintoufag@gmail.com>
2019-10-22 22:42:19 +0600

    Добавил json с фейковыми статьями.

--- src/components/Home/Body/Body.vue
+++ src/components/Home/Body/Body.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <section class="section">
 
-  </div>
+  </section>
 </template>
 
 <script>
--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="home">
-
+  <div class="container">
+    <blog-header></blog-header>
+    <section class="section">
+      <blog-body></blog-body>
+    </section>
+    <blog-footer></blog-footer>
   </div>
 </template>
 
 <script>
+import Header from './Header/Header';
+import Body from './Body/Body';
+import Footer from './Footer/Footer';
+
 export default {
   name: 'Home',
+  components: {
+    'blog-header': Header,
+    'blog-body': Body,
+    'blog-footer': Footer,
+  },
 };
 </script>
-
-<style lang="scss" scoped>
-
-</style>
